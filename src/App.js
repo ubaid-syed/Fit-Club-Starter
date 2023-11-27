6e7303d6f8831b5918b204c358de5faa6e18d1c9
ubaid-syed <arooj.ubaid777@gmail.com>
2023-11-27 21:58:35 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import Footer from './components/Hero/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import Join from './components/Hero/Join/Join';
+import Plans from './components/Hero/Plans/Plans';
+import Programs from './components/Hero/Programs/Programs';
+import Reasons from './components/Hero/Reasons/Reasons';
+import Testimonials from './components/Hero/Testimonials/Testimonials';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+         <Hero/>
+         <Programs/>
+         <Reasons/>
+         <Plans/>
+         <Testimonials/>
+         <Join/>
+         <Footer/>
     </div>
   );
 }
